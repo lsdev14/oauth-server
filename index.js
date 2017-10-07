53a942ea1f57e509310fbfab2ba90caf7f43f7e1
root <you@example.com>
2017-10-07 20:59:18 +0000

    added user route

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var jwt    = require('jsonwebtoken')
+var express = require("express")
+var bodyParser = require("body-parser")
+var jwt    = require("jsonwebtoken")
 var app = express()
 app.use(bodyParser.json())
-app.set('port', (process.env.PORT || 5000))
+app.set("port", (process.env.PORT || 5000))
+app.set("superSecret", "oauthServerLogin");
 
-app.post('/login', function (req, res) {    
+app.post("/login", function (req, res) {    
     console.log("cheking username and password " + new Date().toJSON())
     
     var login = req.body
@@ -15,7 +16,7 @@ app.post('/login', function (req, res) {
             admin: true
         }
         
-        var token = jwt.sign(payload, 'oauthServerLogin', {
+        var token = jwt.sign(payload, app.get("superSecret"), {
             expiresIn: 3600 // expires in 1 hour
         })
 
@@ -31,10 +32,40 @@ app.post('/login', function (req, res) {
     
 })
 
-app.get('/login', function (req, res) {
+app.get("/login", function (req, res) {
     res.send("send a POST to /login with a body { \"username\": \"admin\", \"password\" : \"admin\" } ")
 })
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+app.get("/user", function (req, res) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers["x-access-token"];
+  
+    // decode token
+    if (token) {
+  
+      // verifies secret and checks exp
+      jwt.verify(token, app.get("superSecret"), function(err, decoded) {      
+        if (err) {
+          return res.json({ success: false, message: "Failed to authenticate token." });    
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.decoded = decoded;    
+          next();
+        }
+      });
+  
+    } else {
+  
+      // if there is no token
+      // return an error
+      return res.status(403).send({ 
+          success: false, 
+          message: "No token provided." 
+      });
+  
+    }
+})
+
+app.listen(app.get("port"), function() {
+    console.log("Node app is running on port", app.get("port"));
 })
